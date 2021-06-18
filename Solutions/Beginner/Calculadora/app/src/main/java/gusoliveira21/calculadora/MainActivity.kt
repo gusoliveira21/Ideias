e13a8ab2e18c635bba5790aa1b133bce4d3ff5cf
gusoliveira21 <42920754+gusoliveira21@users.noreply.github.com>
2021-06-18 00:13:25 -0300

    visual calculadora
    foi feito o visual básico, com os números, e os símbolos: +, -, / e ","

---
+++ Solutions/Beginner/Calculadora/app/src/main/java/gusoliveira21/calculadora/MainActivity.kt
@@ -0,0 +1,11 @@
+package gusoliveira21.calculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
